34b144f9752b1df4a3da466d56213713a88b8fce
Kasinathan22 <89632353+Kasinathan22@users.noreply.github.com>
2021-10-22 14:45:24 +0530

    Create 34 prg.java

---
+++ 34 prg.java
@@ -0,0 +1,9 @@
+public class TestSubstring{  
+	 public static void main(String args[]){  
+	String s="Sachin";  
+	System.out.println(s.toUpperCase());//SACHIN  
+	System.out.println(s.toLowerCase());//sachin  
+    System.out.println(s);
+}
+}
+© 2021 GitHub, Inc.
